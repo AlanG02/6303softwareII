ea9de62652013d4c645b5e7bacd0efb1246abf23
AlanG02 <134559337+AlanG02@users.noreply.github.com>
2025-10-09 13:26:46 +0000

    ejecucion

--- PatronesDeDiseno/Herencia/Trabajador.java
+++ PatronesDeDiseno/Herencia/Trabajador.java
@@ -14,4 +14,21 @@ public class Trabajador {
         return 0.0;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
 }
