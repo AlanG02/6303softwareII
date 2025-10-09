441155bd022b5a1b66277a3bbd33186a92c2eeb2
AlanG02 <134559337+AlanG02@users.noreply.github.com>
2025-10-09 13:35:46 +0000

    con arreglos

--- PatronesDeDiseno/Herencia/EjecutarHerencia6303.java
+++ PatronesDeDiseno/Herencia/EjecutarHerencia6303.java
@@ -3,13 +3,30 @@ package PatronesDeDiseno.Herencia;
 public class EjecutarHerencia6303 {
     
     public static void main(String[] args) {
+
         
+
+        // Forma1
+        /* 
         Trabajador objT1 = new Operario(1, "Klinger", 40);
         Trabajador objT2 = new Vendedor(2, "El abuelo", 1000, 0.2);
+        Trabajador objT3 = new Vendedor(3, "Alan", 800, 0.3);
 
         System.out.println(objT1.pagar());
-
         System.out.println(objT2.pagar());
+        System.out.println(objT3.pagar());
+        */
 
+
+        // Forma2
+        Trabajador[] t = new Trabajador[3];
+
+        t[0] = new Operario(1, "Klinger", 40);
+        t[1] = new Vendedor(2, "El abuelo", 1000, 0.2);
+        t[2] = new Vendedor(3, "Alan", 800, 0.3);
+
+        for(int i =0; i < t.length; i++){
+            System.out.println(t[i].pagar());
+        }
     }
 }
